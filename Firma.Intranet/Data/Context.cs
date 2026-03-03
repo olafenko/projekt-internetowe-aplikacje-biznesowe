678ad49fce399e40cbedc5850a196628fbf633a0
olafenko <olopikolo666@gmail.com>
2026-03-03 22:46:56 +0100

    Dodanie pól w klasach Towar i Rodzaj. Wygenerowanie controllera i widoków dla klasy Aktualnosc (zgodnie z wykładem)

---
+++ Firma.Intranet/Data/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Firma.Intranet.Models.CMS;
+
+namespace Firma.Intranet.Data
+{
+    public class Context : DbContext
+    {
+        public Context (DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Firma.Intranet.Models.CMS.Aktualnosc> Aktualnosc { get; set; } = default!;
+    }
+}
--- Firma.Intranet/Program.cs
+++ Firma.Intranet/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Firma.Intranet.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Context") ?? throw new InvalidOperationException("Connection string 'Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
