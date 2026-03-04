9f5ec092053947c85d859260b2edba56f8c12d28
olafenko <olopikolo666@gmail.com>
2026-03-04 17:19:32 +0100

    Wygenerowanie reszty kontrollerów wraz z widokami CRUD. Utworzenie migracji inicjalizacyjnej poleceniem z konsoli nuget Add-Migration InitialCreate, a następnie komenda Update-Database tworzymy baze z migracji (zgodnie z wykladem )

---
+++ Firma.Intranet/Controllers/RodzajController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Firma.Intranet.Data;
+using Firma.Intranet.Models.Sklep;
+
+namespace Firma.Intranet.Controllers
+{
+    public class RodzajController : Controller
+    {
+        private readonly Context _context;
+
+        public RodzajController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Rodzaj
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Rodzaj.ToListAsync());
+        }
+
+        // GET: Rodzaj/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rodzaj = await _context.Rodzaj
+                .FirstOrDefaultAsync(m => m.IdRodzaju == id);
+            if (rodzaj == null)
+            {
+                return NotFound();
+            }
+
+            return View(rodzaj);
+        }
+
+        // GET: Rodzaj/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rodzaj/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdRodzaju,Nazwa,Opis")] Rodzaj rodzaj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rodzaj);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rodzaj);
+        }
+
+        // GET: Rodzaj/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rodzaj = await _context.Rodzaj.FindAsync(id);
+            if (rodzaj == null)
+            {
+                return NotFound();
+            }
+            return View(rodzaj);
+        }
+
+        // POST: Rodzaj/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdRodzaju,Nazwa,Opis")] Rodzaj rodzaj)
+        {
+            if (id != rodzaj.IdRodzaju)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rodzaj);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RodzajExists(rodzaj.IdRodzaju))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rodzaj);
+        }
+
+        // GET: Rodzaj/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rodzaj = await _context.Rodzaj
+                .FirstOrDefaultAsync(m => m.IdRodzaju == id);
+            if (rodzaj == null)
+            {
+                return NotFound();
+            }
+
+            return View(rodzaj);
+        }
+
+        // POST: Rodzaj/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rodzaj = await _context.Rodzaj.FindAsync(id);
+            if (rodzaj != null)
+            {
+                _context.Rodzaj.Remove(rodzaj);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RodzajExists(int id)
+        {
+            return _context.Rodzaj.Any(e => e.IdRodzaju == id);
+        }
+    }
+}
--- Firma.Intranet/Data/Context.cs
+++ Firma.Intranet/Data/Context.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Firma.Intranet.Models.CMS;
+using Firma.Intranet.Models.Sklep;
 
 namespace Firma.Intranet.Data
 {
@@ -15,5 +16,8 @@ namespace Firma.Intranet.Data
         }
 
         public DbSet<Firma.Intranet.Models.CMS.Aktualnosc> Aktualnosc { get; set; } = default!;
+        public DbSet<Firma.Intranet.Models.CMS.Strona> Strona { get; set; } = default!;
+        public DbSet<Firma.Intranet.Models.Sklep.Rodzaj> Rodzaj { get; set; } = default!;
+        public DbSet<Firma.Intranet.Models.Sklep.Towar> Towar { get; set; } = default!;
     }
 }
