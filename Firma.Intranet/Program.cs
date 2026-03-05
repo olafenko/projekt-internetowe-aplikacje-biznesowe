94ff1721399c641a498ce556cb52aabb3b762db9
olafenko <olopikolo666@gmail.com>
2026-03-05 19:37:25 +0100

    Zmiana nazwy bazy danych w connection stringu w Firma.Intranet. Stworzenie migracji bazy danych poleceniem Add-migration <nazwa> w konsoli NuGet, następnie wykonanie tej migracji poleceniem Update-database

--- Firma.Intranet/Program.cs
+++ Firma.Intranet/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Firma.Data.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<FirmaContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Context") ?? throw new InvalidOperationException("Connection string 'Context' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FirmaContext") ?? throw new InvalidOperationException("Connection string 'FirmaContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
