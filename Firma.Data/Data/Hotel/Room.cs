981f0acbd2ab22442537980368d435226a2c489e
olafenko <olopikolo666@gmail.com>
2026-03-07 13:08:00 +0100

    Pierwsza wersja klasy Room

--- Firma.Data/Data/Hotel/Room.cs
+++ Firma.Data/Data/Hotel/Room.cs
@@ -1,10 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Firma.Data.Data.Hotel
 {
     public class Room
     {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Numer pokoju nie może być pusty.")]
+        [Display(Name = "Numer pokoju")]
+        public int Number { get; set; }
+
+        [Required(ErrorMessage = "Piętro nie może być puste.")]
+        [Display(Name = "Piętro")]
+        public int Floor { get; set; }
+
+        [Required(ErrorMessage = "Opis pokoju nie może być pusty.")]
+        [Display(Name = "Opis pokoju")]
+        [Column(TypeName = "nvarchar(max)")]
+        public required string Description { get; set; }
+
+        [Required(ErrorMessage = "Cena nie może być pusta.")]
+        [Display(Name = "Cena za 1 noc")]
+        [Column(TypeName = "money")]
+        public decimal PricePerNight{ get; set; }
+
+        [Required(ErrorMessage = "Zdjęcie pokoju nie może być puste.")]
+        [Display(Name = "Wybierz zdjęcie pokoju")]
+        public required string PhotoUrl{ get; set; }
+
+        [ForeignKey("RoomType")]
+        public int RoomTypeId { get; set; }
+
+        public RoomType? RoomType{ get; set; }
+
+
+        [Required]
+        public bool IsActive { get; set; } = true;
+
+
+
     }
 }
