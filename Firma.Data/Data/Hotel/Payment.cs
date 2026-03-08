a696c0efc373b085f63ea9a88a4c98d28fba44ae
olafenko <olopikolo666@gmail.com>
2026-03-08 18:57:17 +0100

    rozpoczencie implementowania klasy Payment

--- Firma.Data/Data/Hotel/Payment.cs
+++ Firma.Data/Data/Hotel/Payment.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Firma.Data.Data.Hotel
 {
     public class Payment
     {
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Kwota")]
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+
+
+        [ForeignKey("Reservation")]
+        public int ReservationId { get; set; }
+
+        public Reservation? Reservation { get; set; }
+
+        [Required]
+        [Column(TypeName = "datetime")]
+        [Display(Name = "Data płatności")]
+        public DateTime PaymentDate{ get; set; }
+
+
     }
 }
