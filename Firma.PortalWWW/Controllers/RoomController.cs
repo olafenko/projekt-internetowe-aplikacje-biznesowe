5b001859fa66d13b8530f207b84b2bfc36070e82
olafenko <olopikolo666@gmail.com>
2026-05-15 17:40:19 +0200

    commit before refactor

---
+++ Firma.PortalWWW/Controllers/RoomController.cs
@@ -0,0 +1,27 @@
+﻿using Firma.Data.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Firma.PortalWWW.Controllers
+{
+    public class RoomController : Controller
+    {
+
+        private readonly FirmaContext _context;
+
+        public RoomController(FirmaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            ViewBag.RoomModel = await _context.RoomType.Where(r => r.IsActive).ToListAsync();
+            var pageData = await _context.Page.FirstOrDefaultAsync(p => p.IsActive && p.LinkTitle.Equals("POKOJE"));
+
+
+            return View(pageData);
+        }
+    }
+}
