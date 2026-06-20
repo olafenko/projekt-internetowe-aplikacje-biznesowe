e4f94cd676d5b088f69e835a9cb14db767cca5cb
olafenko <olopikolo666@gmail.com>
2026-06-20 21:30:33 +0200

    feature/ amenity controller use service

--- Firma.Interfaces/Hotel/IAmenityService.cs
+++ Firma.Interfaces/Hotel/IAmenityService.cs
@@ -9,6 +9,14 @@ namespace Firma.Interfaces.Hotel
     {
 
         IList<Amenity> GetRoomTypeAmenities(RoomType roomType);
+        Task<IList<Amenity>> GetAllAmenities();
+        Task<Amenity?> GetAmenityById(int id);
+
+        Task CreateAmenity(string name, string description,string icon);
+        Task UpdateAmenity(int id,string name, string description,string icon);
+        Task DeleteAmenity(int id);
+        bool AmenityExists(int id);
+
 
     }
 }
--- Firma.Services/Hotel/AmenityService.cs
+++ Firma.Services/Hotel/AmenityService.cs
@@ -2,6 +2,7 @@
 using Firma.Data.Data.Hotel;
 using Firma.Interfaces.Hotel;
 using Firma.Services.Abstraction;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,10 +15,62 @@ namespace Firma.Services.Hotel
         {
         }
 
+        public bool AmenityExists(int id)
+        {
+
+            return _context.Amenity.Any(e => e.Id == id);
+        }
+
+        public async Task CreateAmenity(string name, string description, string icon)
+        {
+            var amenity = new Amenity{
+                Name = name,
+                Description = description,
+                Icon = icon,
+                IsActive = true
+            };
+
+            _context.Amenity.Add(amenity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAmenity(int id)
+        {
+            
+            var amenity = await _context.Amenity.FirstOrDefaultAsync(a => a.IsActive && a.Id == id);
+
+            amenity.IsActive = false;
+
+            await _context.SaveChangesAsync();
+
+        }
+
+        public async Task<IList<Amenity>> GetAllAmenities()
+        {
+            return await _context.Amenity.Where(a => a.IsActive).ToListAsync();
+        }
+
+        public async Task<Amenity?> GetAmenityById(int id)
+        {
+            return await _context.Amenity.FirstOrDefaultAsync(a=> a.IsActive && a.Id == id);
+        }
+
         public IList<Amenity> GetRoomTypeAmenities(RoomType roomType)
         {
             return roomType.Amenities.Where(a => a.IsActive).ToList();
         }
 
+        public  async Task UpdateAmenity(int id, string name, string description, string icon)
+        {
+            var amenity = await _context.Amenity.FirstOrDefaultAsync(a => a.IsActive && a.Id == id);
+
+            amenity.Name = name;
+            amenity.Description = description;
+            amenity.Icon = icon;
+
+            _context.Update(amenity);
+            await _context.SaveChangesAsync();
+                
+        }
     }
 }
